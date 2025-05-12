6ab414f4e70a310c2ae9139393e914f10fe4e713
benediktstroebl <stroebl@princeton.edu>
2025-05-11 21:29:39 -0700

    update

--- examples/replay_datasets/smpl_example.py
+++ examples/replay_datasets/smpl_example.py
@@ -7,12 +7,14 @@ def experiment(seed=0):
     np.random.seed(seed)
 
     # # example --> you can add as many datasets as you want in the lists!
-    env = ImitationFactory.make("UnitreeH1",
+    env = ImitationFactory.make("SMPLBoxing",
                                 # if SMPL and AMASS are installed, you can use the following:
-                                amass_dataset_conf=AMASSDatasetConf(["DanceDB/DanceDB/20120911_TheodorosSourmelis/Capoeira_Theodoros_v2_C3D_poses"]),
-                                n_substeps=20)
+                                # amass_dataset_conf=AMASSDatasetConf(["DanceDB/DanceDB/20120911_TheodorosSourmelis/Capoeira_Theodoros_v2_C3D_poses"]),
+                                amass_dataset_conf=AMASSDatasetConf(["CMU/CMU/13/13_17_poses"]),
+                                # lafan1_dataset_conf=LAFAN1DatasetConf(["dance2_subject4", "walk1_subject1"]),
+                                n_substeps=15)
 
-    env.play_trajectory(n_episodes=3, n_steps_per_episode=500, render=True)
+    env.play_trajectory(n_episodes=3, n_steps_per_episode=None, render=True)
 
 
 if __name__ == '__main__':
